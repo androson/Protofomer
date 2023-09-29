bfdaa73981afea71368c4b4885653b172368bc48
Mohit Lakhi <mohitdlakhi@hotmail.com>
2023-09-29 22:19:19 +0400

    Jump and Movement
    After watching the class recording agettinging some help
    
    -Made jump work in the same way the lecturer did, also made horizontal movement work
    -with the character rotation when going in the other direction

--- Assets/Mohit/JumpOne.cs
+++ Assets/Mohit/JumpOne.cs
@@ -6,45 +6,89 @@ using UnityEngine;
 public class JumpOne : MonoBehaviour
 {
 
-    [SerializeField] Rigidbody2D playerRB;
-    [SerializeField] float jumpSpeed;
-    float gravityScale;
-    [SerializeField] float gravityMultiplier;
+    [SerializeField] private Rigidbody2D playerRB;
+    //[SerializeField] private float jumpSpeed;
+    //private float gravityScale;
+    //[SerializeField] private float gravityMultiplier;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+        
+    [SerializeField] private float jumpApex;
+    [SerializeField] private float jumpDuration;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        gravityScale = playerRB.gravityScale;
+        //gravityScale = playerRB.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
         Jump();
-        Debug.Log("gravityScale" + gravityScale);
-        Debug.Log("gravityMultiplier" + gravityMultiplier);
-        Debug.Log("playerRB.velocity.y" + playerRB.velocity.y);
+       //Debug.Log("gravityScale" + gravityScale);
+       //Debug.Log("gravityMultiplier" + gravityMultiplier);
+       //Debug.Log("playerRB.velocity.y" + playerRB.velocity.y);
 
     }
 
+
+    private void Movement()
+    {
+        // Gets left and right movement input on x axis from horizontal keys be it arrow or A D,
+        float inputx = Input.GetAxis("Horizontal");
+
+        // makes velocity of horizontal movement into walkspeed while freezing vertical movement with velocity.y
+        playerRB.velocity = new Vector2(inputx * walkSpeed, playerRB.velocity.y);
+        
+        //if input is great than 0face right 
+        if (inputx> 0)
+        {
+            transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        // if input is great than face left 
+        if (inputx < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+    }
     private void Jump()
     {
+        // defining gravity by multiplying -2 into max jump hieght divided by jumpduration multiplied by jumpduration
+        float grav = -2 * jumpApex / (jumpDuration * jumpDuration);
+        
+        
+
+        //Debug.Log("playerRB velocity y" + playerRB.velocity.y); works
+
+        // when player jumps Velocity.y is positive and when he is descending it goes negative 
+        //so when he is descending gravity is doubled to make fall faster 
+
+        if (playerRB.velocity.y < -0.1)
+        {
+            grav = grav * 2; // not workin
+            //Debug.Log("i work"); works 
+        }
+        
+        float velo = -grav * jumpDuration;
+
+        Physics2D.gravity = new Vector2(0, grav);
+
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            playerRB.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            //jump up 
+            playerRB.velocity = Vector2.up * velo;
 
-            if (playerRB.velocity.y > 0)
-            {
-                playerRB.gravityScale = gravityScale * gravityMultiplier;
+            //playerRB.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
 
-            }
-
-            else
-            {
-                playerRB.gravityScale = gravityScale;
-            }
+            
+            //
+            //else
+            //{
+            //    playerRB.gravityScale = gravityScale;
+            //}
         }
         
        
